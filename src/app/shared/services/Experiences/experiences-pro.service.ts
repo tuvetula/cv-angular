e1e032795d3aec51bd95b4e09f561d03dee2b6a5
tuvetula <romain.marcant@gmail.com>
2020-12-05 14:41:12 +0100

    add eurécia experience

--- src/app/shared/services/Experiences/experiences-pro.service.ts
+++ src/app/shared/services/Experiences/experiences-pro.service.ts
@@ -8,19 +8,28 @@ export class ExperiencesProService {
   constructor() {
     this.listExperiences = [
       {
+        name: 'Développeur web',
+        company: 'Eurécia',
+        city: 'Castanet Tolosan',
+        country: 'France',
+        startDate: new Date(2020,10),
+        endDate: new Date(2021,3),
+        description: ['Développement des évolutions et maintenance sur le CRM interne (Laravel, Javascript, html/css)' ]
+      },
+      {
         name: 'Stage concepteur développeur Web',
         company: 'As creation',
         city: 'Saint-orens-de-gameville',
         country: 'France',
         startDate: new Date(2019, 7),
         endDate: new Date(2019, 9),
-        description:["Conception et développement d'une application web front-end et back-end en html/css, javascript, php.","Développement d'un outil de diffusion automatique d'annonces avec la librairie puppeteer (javascript)"]
+        description: ['Conception et développement d\'une application web front-end et back-end en html/css, javascript, php.', 'Développement d\'un outil de diffusion automatique d\'annonces avec la librairie puppeteer (javascript)']
       },
       {
         name: 'Technicien itinérant de maintenance monétique',
         company: 'Verifone',
         city: 'Midi-pyrénées',
-        country: "France",
+        country: 'France',
         startDate: new Date(2011,0),
         endDate: new Date(2018,10),
         description: ['Maintenance sur site de Terminaux de Paiements Electroniques.','Déploiement et mise en réseau des solutions de paiement + formation utilisateurs.','Paramétrage et formation clients sur logiciel Turbo (transfert de flux bancaires)']
