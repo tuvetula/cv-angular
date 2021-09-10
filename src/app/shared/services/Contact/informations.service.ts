27c9dcb3162ccd7f518e202113804b3c38879808
tuvetula <romain.marcant@gmail.com>
2021-09-10 19:59:04 +0200

    feat: modify address

--- src/app/shared/services/Contact/informations.service.ts
+++ src/app/shared/services/Contact/informations.service.ts
@@ -10,17 +10,17 @@ export class InformationsService {
   constructor() {
     this.listInformations = {
       address: {
-        street: "62, route de saint-ybars",
-        line1: "Lieu-dit Lamourère",
-        zipcode: "31190",
-        city: "Esperce",
-        country: "France"
+        street: '110, avenue de Toulouse',
+        line1: null,
+        zipcode: '31750',
+        city: 'Escalquens',
+        country: 'France'
       },
-      phoneNumber: "06 03 91 86 13",
-      mail: "romain.marcant@gmail.com",
-      linkedIn: "https://www.linkedin.com/in/romain-marcant/",
-      github: "https://github.com/tuvetula",
-      cv: "../../assets/Pdf/cv_Marcant_Romain.pdf",
+      phoneNumber: '06 03 91 86 13',
+      mail: 'romain.marcant@gmail.com',
+      linkedIn: 'https://www.linkedin.com/in/romain-marcant/',
+      github: 'https://github.com/tuvetula',
+      cv: '../../assets/Pdf/cv_Marcant_Romain.pdf',
       dateOfBirth: new Date(1982,5,17),
       nbOfChildrens: 2,
 
